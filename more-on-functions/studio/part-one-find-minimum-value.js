f78315f86bdfa922f17cf2eba9a11880d23920c9
HenryLuepke <henryluepke4@gmail.com>
2024-07-01 20:38:42 -0500

    Class 6 Studio

--- more-on-functions/studio/part-one-find-minimum-value.js
+++ more-on-functions/studio/part-one-find-minimum-value.js
@@ -7,4 +7,17 @@ let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
 //Using one of the test arrays as the argument, call your function inside the console.log statement below.
 
-console.log(/* your code here */);
+function minValue(arr) {
+    let min = arr[0]
+    for (let i = 1; i < arr.length; i++){
+        if (arr[i] < min) {
+            min = arr[i]
+        }
+    }
+
+    return min
+}
+
+console.log(minValue(nums1));
+console.log(minValue(nums2));
+console.log(minValue(nums3));
--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -20,6 +20,18 @@ function findMinValue(arr){
 
 //Your function here...
 
+function sort(arr) {
+  let sorted = [];
+  
+  while (arr.length > 0) {
+    sorted.push(findMinValue(arr));
+    let index = arr.indexOf(findMinValue(arr));
+    arr.splice(index, 1);
+  }
+
+  return sorted
+}
+
 /* BONUS MISSION: Refactor your sorting function to use recursion below:
  */
 
@@ -27,3 +39,7 @@ function findMinValue(arr){
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+
+console.log(sort(nums1));
+console.log(sort(nums2));
+console.log(sort(nums3));
