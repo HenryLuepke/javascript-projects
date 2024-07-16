47f3ad3c9f45b57b11129e3d1e369f46bf701089
HenryLuepke <henryluepke4@gmail.com>
2024-07-15 19:57:56 -0500

    Class 9 studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,5 +1,22 @@
 
 let launchcode = {
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["WebDev", "Data Science", "Salesforce"],
+    launchOutput: function(num) {
+        if (num % 2 === 0 && num % 3 === 0) {
+            return "LaunchCode!"
+        } else if (num % 2 === 0 && num % 5 === 0) {
+            return 'Launch Rocks! (CRASH!!!!)'
+        } else if (num % 2 === 0) {
+            return "Launch!"
+        } else if (num % 3 === 0) {
+            return "Code!"
+        } else if (num % 5 === 0) {
+            return "Rocks!"
+        }
+    }
   
 }
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,55 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
+  describe("launchcode", function() {
+    
+    test("Is value of organization nonprofit", function() {
+      let output = launchcode.organization
+      expect(output).toBe("nonprofit")
+    })
+
+    test("Is executive director Jeff", function() {
+      let output = launchcode.executiveDirector
+      expect(output).toBe("Jeff")
+    })
+
+    test("Is percentage 100", function() {
+      let output = launchcode.percentageCoolEmployees
+      expect(output).toBe(100)
+    })
+
+    test("Does programs offered contain the correct values", function() {
+      let arr = launchcode.programsOffered
+      expect(arr[0]).toBe("WebDev")
+      expect(arr[1]).toBe("Data Science")
+      expect(arr[2]).toBe("Salesforce")
+      expect(arr.length).toBe(3)
+    })
+
+    test("When passed a number only divisible by 2 the function returns 'Launch!", function() {
+      let output = launchcode.launchOutput(4);
+      expect(output).toBe("Launch!")
+    })
+
+    test("When passed a number only divisible by 3 the function returns 'Code!", function() {
+      let output = launchcode.launchOutput(9);
+      expect(output).toBe("Code!")
+    })
+
+    test("When passed a number only divisible by 5 the function returns 'Rocks!", function() {
+      let output = launchcode.launchOutput(25);
+      expect(output).toBe("Rocks!")
+    })
+
+    test("When passed a number only divisible by 2 and 3 the function returns 'Launchcode!", function() {
+      let output = launchcode.launchOutput(6);
+      expect(output).toBe("LaunchCode!")
+    })
+
+    test("When passed a number only divisible by 2 and 5 the function returns 'Launch Rocks! (CRASH!!!!)'", function() {
+      let output = launchcode.launchOutput(10);
+      expect(output).toBe('Launch Rocks! (CRASH!!!!)')
+    })
+  })
   
 });
